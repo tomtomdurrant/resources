056b8a2353cadac86c274cbc5e97c90453ee2df8
Tom Durrant <tomchdurrant@gmail.com>
2020-09-13 21:46:04 +0100

    Remove duplicated files

--- benday-visual-studio-csharp-snippets/VisualStudio/_copy-to-windows-vs2019-snippets-directory.bat
+++
@@ -1,1 +0,0 @@
-copy /y *.snippet "%userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
--- benday-visual-studio-csharp-snippets/VisualStudio/_open-windows-vs2019-snippets-directory.bat
+++
@@ -1,1 +0,0 @@
-explorer %userprofile%\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets
